6c1a92d08703ee23c5f2a5dddb4eff439fa9b275
florcaro <flor_caro17@live.com.ar>
2022-02-23 16:13:15 -0300

    error solucionado

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RegistroComponent } from './registro/registro.component';
 import { EntradaComponent } from './entrada/entrada.component';
 
-
-
+@NgModule({
+    declarations: [ EntradaComponent,RegistroComponent
+    ],
+    imports:[
+        CommonModule
+    ],
+  })
+  export class FormsModule { }
 
