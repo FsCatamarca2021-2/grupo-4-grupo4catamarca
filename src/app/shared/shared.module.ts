f71c6fd66dc9f305747b9664726671586bc4845b
Gonzalo Rodriguez <gachr02@gmail.com>
2022-02-23 13:35:41 -0300

    mod. de navbar y creacion de c inicio

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent  ],
+  declarations: [HeaderComponent, InicioComponent  ],
   imports: [ 
     CommonModule
     ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent,
+    InicioComponent]
 })
 export class SharedModule { }
