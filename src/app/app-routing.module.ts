63f40d741a5c90308df217969de315988c894154
florcaro <flor_caro17@live.com.ar>
2022-03-28 18:10:08 -0300

    Inicio de sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,18 @@ import { RegistroComponent } from './forms/registro/registro.component';
 import { ContactoComponent } from './pag/contacto/contacto.component';
 import { QuienesSomosComponent } from './pag/quienes-somos/quienes-somos.component';
 import { InicioComponent } from './shared/inicio/inicio.component';
-
-
+import { IncioSesionComponent } from './pag/incio-sesion/incio-sesion.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 
 const routes: Routes = [
   {path:"", component: InicioComponent},
+  {path:'header', component:HeaderComponent},
   {path:'entrada', component: EntradaComponent},
   {path:'registro', component: RegistroComponent},
   {path:'quienes_somos', component: QuienesSomosComponent},
   {path:'contacto', component: ContactoComponent},
- 
+  {path:'inicio-sesion',component: IncioSesionComponent}
 
 ];
 
