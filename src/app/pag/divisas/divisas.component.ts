4f68d9d6b1d2deb835cff885f4f3c7e8a8e7db41
florcaro <flor_caro17@live.com.ar>
2022-04-25 00:02:37 -0300

    cambio de divisas

---
+++ src/app/pag/divisas/divisas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divisas',
+  templateUrl: './divisas.component.html',
+  styleUrls: ['./divisas.component.css']
+})
+export class DivisasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
